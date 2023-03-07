dc3ac55c955823c364bf616cf5cd19b8219cc0de
IghorBr <ighorbruno20@gmail.com>
2023-03-07 15:46:30 -0300

    jenkins

--- src/main/java/ibn/api/health/api/model/input/HistoricoInput.java
+++ src/main/java/ibn/api/health/api/model/input/HistoricoInput.java
@@ -39,4 +39,74 @@ public class HistoricoInput implements Serializable {
     @NotNull
     @Schema(example = "75")
     private Double peso;
+
+    HistoricoInput(@NotNull String observacoes, @NotNull Integer pressaoSistolica, @NotNull Integer pressaoDiastolica, @NotNull Double bpm, @NotNull Double glicose, @NotNull Double altura, @NotNull Double peso) {
+        this.observacoes = observacoes;
+        this.pressaoSistolica = pressaoSistolica;
+        this.pressaoDiastolica = pressaoDiastolica;
+        this.bpm = bpm;
+        this.glicose = glicose;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public static HistoricoInputBuilder builder() {
+        return new HistoricoInputBuilder();
+    }
+
+    public static class HistoricoInputBuilder {
+        private @NotNull String observacoes;
+        private @NotNull Integer pressaoSistolica;
+        private @NotNull Integer pressaoDiastolica;
+        private @NotNull Double bpm;
+        private @NotNull Double glicose;
+        private @NotNull Double altura;
+        private @NotNull Double peso;
+
+        HistoricoInputBuilder() {
+        }
+
+        public HistoricoInputBuilder observacoes(@NotNull String observacoes) {
+            this.observacoes = observacoes;
+            return this;
+        }
+
+        public HistoricoInputBuilder pressaoSistolica(@NotNull Integer pressaoSistolica) {
+            this.pressaoSistolica = pressaoSistolica;
+            return this;
+        }
+
+        public HistoricoInputBuilder pressaoDiastolica(@NotNull Integer pressaoDiastolica) {
+            this.pressaoDiastolica = pressaoDiastolica;
+            return this;
+        }
+
+        public HistoricoInputBuilder bpm(@NotNull Double bpm) {
+            this.bpm = bpm;
+            return this;
+        }
+
+        public HistoricoInputBuilder glicose(@NotNull Double glicose) {
+            this.glicose = glicose;
+            return this;
+        }
+
+        public HistoricoInputBuilder altura(@NotNull Double altura) {
+            this.altura = altura;
+            return this;
+        }
+
+        public HistoricoInputBuilder peso(@NotNull Double peso) {
+            this.peso = peso;
+            return this;
+        }
+
+        public HistoricoInput build() {
+            return new HistoricoInput(observacoes, pressaoSistolica, pressaoDiastolica, bpm, glicose, altura, peso);
+        }
+
+        public String toString() {
+            return "HistoricoInput.HistoricoInputBuilder(observacoes=" + this.observacoes + ", pressaoSistolica=" + this.pressaoSistolica + ", pressaoDiastolica=" + this.pressaoDiastolica + ", bpm=" + this.bpm + ", glicose=" + this.glicose + ", altura=" + this.altura + ", peso=" + this.peso + ")";
+        }
+    }
 }
